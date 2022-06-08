65a168c7d50f3d38ffb7b653a05da5b4fa1323c1
richiaidil <106723859+richiaidil@users.noreply.github.com>
2022-06-08 21:54:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
+
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
@@ -27,9 +27,9 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+iris = read.csv(text=GET("https://raw.githubusercontent.com/richiaidil/aaa-asignment/main/IRIS.csv"))
+X = iris['sepal_length','sepal_width', 'petal_length','petal_width']
+Y = iris.species
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
@@ -38,11 +38,12 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
+iris.target_names = ['setosa','versicolor','virginica']
 st.write(iris.target_names)
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
+#st.write(iris.target_names[prediction])
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
