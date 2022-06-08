59f788c2848fe3ef1844a0e65cd7e9e8b8883156
richiaidil <106723859+richiaidil@users.noreply.github.com>
2022-06-08 16:25:57 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,47 +1,48 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-import time
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
 
-st.header("My first Streamlit App")
+st.write("""
+# Simple Iris Flower Prediction App
+This app predicts the **Iris flower** type!
+""")
 
-option = st.sidebar.selectbox(
-    'Select a mini project',
-     ['line chart','map','T n C','Long Process'])
+st.sidebar.header('User Input Parameters')
 
-if option=='line chart':
-    chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
 
-    st.line_chart(chart_data)
+df = user_input_features()
 
-elif option=='map':
-    map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+st.subheader('User Input parameters')
+st.write(df)
 
-    st.map(map_data)
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
 
-elif option=='T n C':
-    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-    show = st.checkbox('I agree the terms and conditions')
-    if show:
-        st.write(pd.DataFrame({
-        'Intplan': ['yes', 'yes', 'yes', 'no'],
-        'Churn Status': [0, 0, 0, 1]
-        }))
+clf = RandomForestClassifier()
+clf.fit(X, Y)
 
-else:
-    'Starting a long computation...'
-    
-    latest_iteration = st.empty()
-    bar = st.progress(0)
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
 
-    for i in range(100):
-   
-        latest_iteration.text(f'Iteration {i+1}')
-        bar.progress(i + 1)
-        time.sleep(0.1)
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
 
-    '...and now we\'re done!'
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+#st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
